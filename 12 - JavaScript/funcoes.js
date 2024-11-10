775c1ef256abf01c4e49c1da5561222ac3bbcc7b
wagnernazarios <wagnernazario124@gmail.com>
2024-11-10 20:44:25 -0300

    o basico de function finalizado

--- 12 - JavaScript/funcoes.js
+++ 12 - JavaScript/funcoes.js
@@ -16,3 +16,58 @@ function num() {
 console.log(`y fora da função é: ${y}`);
 
 num();
+
+// arrow function
+
+const testantoArrow = ((n) => {
+  if (n % 2 === 0) {
+    console.log("Esse número é par");
+    return;
+  }
+  console.log("esse numero é impar");
+})(12);
+
+const raizQuadrada = (x) => {
+  return x * x;
+};
+
+console.log(raizQuadrada(6));
+
+const multi = (m, n) => {
+  if (n == undefined) {
+    return m * 2;
+  } else {
+    return m * n;
+  }
+};
+
+console.log(multi(15));
+console.log(multi(6, 8));
+
+// closure
+
+function somefunction() {
+  let text = "Alguma coisa";
+
+  function display() {
+    console.log(text);
+  }
+
+  display();
+}
+
+somefunction();
+
+// recursion
+
+(function untilTen(n, m) {
+  if (n < 10) {
+    console.log("A função para de funcionar!");
+  } else {
+    let x = n - m;
+
+    console.log(x);
+
+    untilTen(x, m);
+  }
+})(140, 10);
