7206264e567a560fc6776d9add867f36dbde415e
wagnernazarios <wagnernazario124@gmail.com>
2025-01-03 06:31:22 -0300

    Tabuada finalizada

---
+++ 13 - 6º projeto tabuada/src/index.js
@@ -0,0 +1,50 @@
+//Seleção de elementos
+
+const multiplicationForm = document.querySelector("#multiplication-form")
+const numberInput = document.querySelector("#number")
+const multiplication = document.querySelector("#multiplicator")
+const multiplicationTitle = document.querySelector(".multiplication-title span")
+const multiplicationTable = document.querySelector(".multiplication-operation")
+
+// Funções
+
+const createTable = (number, multiplicatorNumber) =>{
+
+    multiplicationTable.innerHTML = "";
+
+    for(let i = 1; i <= multiplicatorNumber; i++){
+
+        const result = number *  i; 
+
+        const template = ` <div class="row">
+                <div class="operation"> ${number} x ${i} =  </div> 
+                <div class="result">  ${result} </div>
+            </div>`;
+
+        const parser = new DOMParser();
+
+        const htmlTemplate = parser.parseFromString(template, 'text/html')
+
+        const row = htmlTemplate.querySelector('.row')
+
+        multiplicationTable.appendChild(row) 
+    }
+
+    multiplicationTitle.innerHTML = number;
+
+}
+
+
+// Eventos
+
+
+multiplicationForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+    const multiplicationNumber = +numberInput.value;
+    const multiplicatorNumber = +multiplication.value;
+
+if(!multiplicationNumber || !multiplicatorNumber) return;
+
+    createTable(multiplicationNumber, multiplicatorNumber)
+})
