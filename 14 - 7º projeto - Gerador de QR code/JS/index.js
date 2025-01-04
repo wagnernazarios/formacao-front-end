ba8d790be84dc910dd93d5ff0b413e3dd861eb4d
wagnernazarios <wagnernazario124@gmail.com>
2025-01-03 23:51:35 -0300

    projeto gerdaor de qr code finalizado!

---
+++ 14 - 7º projeto - Gerador de QR code/JS/index.js
@@ -0,0 +1,44 @@
+const container = document.querySelector('.container')
+const btn = document.querySelector('.qr-form button')
+const input = document.querySelector('.qr-form input')
+const qrCodeImg = document.querySelector('.qr-code img')
+const qrCode = document.querySelector('.qr-code')
+
+function generatorQrCode() {
+    const inputValue = input.value;
+
+    if (!inputValue) return;
+
+    btn.innerText = 'Gerando QR Code...'
+
+    qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`
+
+    container.classList.add('active')
+
+    qrCodeImg.addEventListener('load', () => {
+        qrCode.classList.add('active')
+
+        btn.innerText = 'QR Code criado!'
+
+
+    })
+
+}
+
+btn.addEventListener('click', () => {
+    generatorQrCode()
+})
+
+input.addEventListener('keydown', (e) => {
+    if (e.code === 'Enter') {
+        generatorQrCode()
+    }
+})
+
+input.addEventListener('keyup', () => {
+    if (!input.value) {
+        container.classList.remove('active')
+        qrCode.classList.remove('active')
+        btn.innerText = 'Gerar QR Code'
+    }
+})
