060e12a9d9a58e0a22b87a1c028f99ce90483318
wagnernazarios <wagnernazario124@gmail.com>
2025-04-27 22:53:09 -0300

    iniciando o js

---
+++ 16 - 9º projeto IMC/js/script.js
@@ -0,0 +1,31 @@
+//IMC DATA
+const data = [
+    {
+        min: 0,
+        max: 18.5,
+        classification: "menor que 18,5",
+        info: "Magreza",
+        Obesity: 0
+    },
+    {
+        min: 18.5,
+        max: 24.9,
+        classification: "entre 18,5 e 24,9",
+        info: "Normal",
+        Obesity: 0
+    },
+    {
+        min: 25,
+        max: 29.9,
+        classification: "entre 25 e 29,9",
+        info: "Sobrepeso",
+        Obesity: 1
+    },
+    {
+        min: 30,
+        max: 39.9,
+        classification: "entre 30 e 39,9",
+        info: "Obeso",
+        Obesity: 2
+    },
+]
