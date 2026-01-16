7e0385f23615f2dff67a1513ab98535a75e2f000
wagnernazarios <wagnernazario124@gmail.com>
2026-01-16 20:12:37 -0300

    if ternario com react

--- react-avancado/react-avancado/src/ConditionRender.jsx
+++ react-avancado/react-avancado/src/ConditionRender.jsx
@@ -3,10 +3,25 @@ import React from 'react'
 const ConditionRender = () => {
 
     const x = true;
-  return (
+    const nome = "Wagner"
+
+  return (    
+
     <div>
+      {/*renderização condicional */}
         <h3>Isso será renderizado condicionalmente</h3>
         {x && <p>Se x for true sim</p>}
+
+      {/*if ternario */}
+      {nome === "Wagner" ? (
+        <div>
+          <p>O nome é Wagner</p>
+        </div>
+      ) : (
+        <div>
+          <p>Nome não encontrado</p>
+        </div>
+      )}
     </div>
   ) 
 }
